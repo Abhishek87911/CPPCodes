43107a2e2ac93e2f5e5e425c432fbd173c64f30d
Abhishek Gupta <bsaagupta77@gmail.com>
2024-03-27 14:09:54 +0530

    added codes

---
+++ DSU.cpp
@@ -0,0 +1,43 @@
+class DSU {
+    vector<int>  parent, size; 
+    int mx=0;
+    int noOfComponents;
+public: 
+    DSU(int n) {
+        
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+            noOfComponents=n;
+        }
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+            mx=max(mx,size[ulp_u]);
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+            mx=max(mx,size[ulp_v]);
+        }
+        noOfComponents--;
+    }
+  int giveNoOfComponenets() {
+    return noOfComponents;
+  }
+
+};
